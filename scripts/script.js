1613d979d117075c28d58305c38280ab6cb05c59
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-10 22:07:05 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const menuToggle = document.getElementById("hamburgerIcon");
-let mobileLinks = document.getElementById("mobile-nav-items");
+let mobileLinks = document.querySelector("mobile-nav-items");
 
 menuToggle.addEventListener('click', () => {
    mobileLinks.classList.toggle('active');
-   console.log(mobileLinks.classList);
 })
